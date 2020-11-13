5a138b0368d79acede61ebef19088044954f55db
yyjim <yy.jim731@gmail.com>
2020-11-13 17:47:34 +0800

    Updated XcodeIconsExporter and test cases

--- Sources/XcodeExport/XcodeIconsExporter.swift
+++ Sources/XcodeExport/XcodeIconsExporter.swift
@@ -22,53 +22,5 @@ final public class XcodeIconsExporter: XcodeImagesExporterBase {
 
         return [contentsFile] + imageAssetsFiles + extensionFiles
     }
-
-    public func export(assets: [Image], append: Bool) throws -> [FileContents] {
-        var files: [FileContents] = []
-        
-        // Assets.xcassets/Icons/Contents.json
-        let contentsJson = XcodeEmptyContents()
-        files.append(FileContents(
-            destination: Destination(directory: output.assetsFolderURL, file: contentsJson.fileURL),
-            data: contentsJson.data
-        ))
-        
-        try assets.forEach { image in
-            // Create directory for imageset
-            let dirURL = output.assetsFolderURL.appendingPathComponent("\(image.name).imageset")
-
-            // Write PDF to imageset directory
-            let imageURL = URL(string: "\(image.name).\(image.format)")!
-            
-            files.append(FileContents(
-                destination: Destination(directory: dirURL, file: imageURL),
-                sourceURL: image.url
-            ))
-            
-            let preservesVector = output.preservesVectorRepresentation?.first(where: { $0 == image.name }) != nil
-
-            let idiom = image.idiom.flatMap({ XcodeAssetIdiom(rawValue: $0) }) ?? .universal
-            
-            // Assets.xcassets/Icons/***.imageset/Contents.json
-            let contents = XcodeAssetContents(
-                icons: [XcodeAssetContents.ImageData(idiom: idiom, filename: "\(image.name).\(image.format)")],
-                preservesVectorRepresentation: preservesVector
-            )
-            let encoder = JSONEncoder()
-            encoder.outputFormatting = .prettyPrinted
-            let data = try encoder.encode(contents)
-            let fileURL = URL(string: "Contents.json")!
-            files.append(FileContents(
-                destination: Destination(directory: dirURL, file: fileURL),
-                data: data
-            ))
-        }
-        
-        let imageNames = assets.map { $0.name }
-        
-        let extensionFiles = try generateExtensions(names: imageNames, append: append)
-        files.append(contentsOf: extensionFiles)
-        
-        return files
-    }
+    
 }
