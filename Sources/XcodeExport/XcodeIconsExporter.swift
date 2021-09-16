0141c32fd275e57ed664e60b2524c90d6b2edda7
Aleksei Kakoulin <alexey1312ru@gmail.com>
2021-09-17 05:02:21 +0700

    update XcodeIconsExporter

--- Sources/XcodeExport/XcodeIconsExporter.swift
+++ Sources/XcodeExport/XcodeIconsExporter.swift
@@ -10,10 +10,23 @@ final public class XcodeIconsExporter: XcodeImagesExporterBase {
         // Generate assets
         let assetsFolderURL = output.assetsFolderURL
         let preservesVectorRepresentation = output.preservesVectorRepresentation
+        // Filtering at suffixes
         let renderMode = output.renderMode ?? .template
+        let defaultSuffix = renderMode == .template ? output.renderModeDefaultSuffix : nil
+        let originalSuffix = renderMode == .template ? output.renderModeOriginalSuffix : nil
+        let templateSuffix = renderMode != .template ? output.renderModeTemplateSuffix : nil
 
         let imageAssetsFiles = try icons.flatMap { imagePack -> [FileContents] in
             let preservesVector = preservesVectorRepresentation?.first(where: { $0 == imagePack.light.name }) != nil
+
+            if let defaultSuffix = defaultSuffix, imagePack.light.name.hasSuffix(defaultSuffix) {
+                return try imagePack.makeFileContents(to: assetsFolderURL, preservesVector: preservesVector, renderMode: .default)
+            } else if let originalSuffix = originalSuffix, imagePack.light.name.hasSuffix(originalSuffix) {
+                return try imagePack.makeFileContents(to: assetsFolderURL, preservesVector: preservesVector, renderMode: .original)
+            } else if let templateSuffix = templateSuffix, imagePack.light.name.hasSuffix(templateSuffix) {
+                return try imagePack.makeFileContents(to: assetsFolderURL, preservesVector: preservesVector, renderMode: .template)
+            }
+
             return try imagePack.makeFileContents(to: assetsFolderURL, preservesVector: preservesVector, renderMode: renderMode)
         }
 
