3588c5b998a6c33896c3ee5c96c21ef6b29ffb88
Semen Kologrivov <firedragon855@gmail.com>
2022-01-19 17:24:29 +0700

    fix

--- Sources/XcodeExport/XcodeTypographyExporter/Style/XcodeTypographyExporter+SQStyleShared.swift
+++ Sources/XcodeExport/XcodeTypographyExporter/Style/XcodeTypographyExporter+SQStyleShared.swift
@@ -64,89 +64,89 @@ extension XcodeTypographyExporter {
 
     func strikethroughTypes(forStyle style: String) -> String {
         """
-        @objc lazy var strikethroughStyleSingle: \(style) = {
-            self.strikethroughStyle = .single
-            return self
-        }()
+            @objc lazy var strikethroughStyleSingle: \(style) = {
+                self.strikethroughStyle = .single
+                return self
+            }()
 
-        @objc lazy var strikethroughStyleThick: \(style) = {
-            self.strikethroughStyle = .thick
-            return self
-        }()
+            @objc lazy var strikethroughStyleThick: \(style) = {
+                self.strikethroughStyle = .thick
+                return self
+            }()
 
-        @objc lazy var strikethroughStyleDouble: \(style) = {
-            self.strikethroughStyle = .double
-            return self
-        }()
+            @objc lazy var strikethroughStyleDouble: \(style) = {
+                self.strikethroughStyle = .double
+                return self
+            }()
 
-        @objc lazy var strikethroughStylePatternDot: \(style) = {
-            self.strikethroughStyle = .patternDot
-            return self
-        }()
+            @objc lazy var strikethroughStylePatternDot: \(style) = {
+                self.strikethroughStyle = .patternDot
+                return self
+            }()
 
-        @objc lazy var strikethroughStylePatternDash: \(style) = {
-            self.strikethroughStyle = .patternDash
-            return self
-        }()
+            @objc lazy var strikethroughStylePatternDash: \(style) = {
+                self.strikethroughStyle = .patternDash
+                return self
+            }()
 
-        @objc lazy var strikethroughStylePatternDashDot: \(style) = {
-            self.strikethroughStyle = .patternDashDot
-            return self
-        }()
+            @objc lazy var strikethroughStylePatternDashDot: \(style) = {
+                self.strikethroughStyle = .patternDashDot
+                return self
+            }()
 
-        @objc lazy var strikethroughStylePatternDashDotDot: \(style) = {
-            self.strikethroughStyle = .patternDashDotDot
-            return self
-        }()
+            @objc lazy var strikethroughStylePatternDashDotDot: \(style) = {
+                self.strikethroughStyle = .patternDashDotDot
+                return self
+            }()
 
-        @objc lazy var strikethroughStyleByWord: \(style) = {
-            self.strikethroughStyle = .byWord
-            return self
-        }()
+            @objc lazy var strikethroughStyleByWord: \(style) = {
+                self.strikethroughStyle = .byWord
+                return self
+            }()
         """
     }
 
     func underlineTypes(forStyle style: String) -> String {
         """
-        @objc lazy var underlineStyleSingle: \(style) = {
-            self.underlineStyle = .single
-            return self
-        }()
+            @objc lazy var underlineStyleSingle: \(style) = {
+                self.underlineStyle = .single
+                return self
+            }()
 
-        @objc lazy var underlineStyleThick: \(style) = {
-            self.underlineStyle = .thick
-            return self
-        }()
+            @objc lazy var underlineStyleThick: \(style) = {
+                self.underlineStyle = .thick
+                return self
+            }()
 
-        @objc lazy var underlineStyleDouble: \(style) = {
-            self.underlineStyle = .double
-            return self
-        }()
+            @objc lazy var underlineStyleDouble: \(style) = {
+                self.underlineStyle = .double
+                return self
+            }()
 
-        @objc lazy var underlineStylePatternDot: \(style) = {
-            self.underlineStyle = .patternDot
-            return self
-        }()
+            @objc lazy var underlineStylePatternDot: \(style) = {
+                self.underlineStyle = .patternDot
+                return self
+            }()
 
-        @objc lazy var underlineStylePatternDash: \(style) = {
-            self.underlineStyle = .patternDash
-            return self
-        }()
+            @objc lazy var underlineStylePatternDash: \(style) = {
+                self.underlineStyle = .patternDash
+                return self
+            }()
 
-        @objc lazy var underlineStylePatternDashDot: \(style) = {
-            self.underlineStyle = .patternDashDot
-            return self
-        }()
+            @objc lazy var underlineStylePatternDashDot: \(style) = {
+                self.underlineStyle = .patternDashDot
+                return self
+            }()
 
-        @objc lazy var underlineStylePatternDashDotDot: \(style) = {
-            self.underlineStyle = .patternDashDotDot
-            return self
-        }()
+            @objc lazy var underlineStylePatternDashDotDot: \(style) = {
+                self.underlineStyle = .patternDashDotDot
+                return self
+            }()
 
-        @objc lazy var underlineStyleByWord: \(style) = {
-            self.underlineStyle = .byWord
-            return self
-        }()
+            @objc lazy var underlineStyleByWord: \(style) = {
+                self.underlineStyle = .byWord
+                return self
+            }()
         """
     }
 
