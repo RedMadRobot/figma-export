ab3890545418fb16bac3117b4744807b1a6ec917
alexey1312 <alexey1312ru@gmail.com>
2022-03-28 11:49:02 +0600

    Fix error description to AssetsValidatorError

--- Sources/FigmaExportCore/Processor/AssetsValidatorError.swift
+++ Sources/FigmaExportCore/Processor/AssetsValidatorError.swift
@@ -17,9 +17,9 @@ enum AssetsValidatorError: LocalizedError {
         case .countMismatch(let light, let dark):
             error = "The number of assets doesn’t match. Light theme contains \(light), and dark \(dark)."
         case .countMismatchLightHighContrastColors(let light, let lightHC):
-            error = "The number of assets doesn’t match. Light color palette contains theme contains \(light), and light high contrast color palette \(lightHC)."
+            error = "The number of assets doesn’t match. Light color palette contains \(light), and light high contrast color palette \(lightHC)."
         case .countMismatchDarkHighContrastColors(let dark, let darkHC):
-            error = "The number of assets doesn’t match. Dark color palette contains theme contains \(dark), and dark high contrast color palette \(darkHC)."
+            error = "The number of assets doesn’t match. Dark color palette contains \(dark), and dark high contrast color palette \(darkHC)."
         case .darkAssetsNotFoundInLightPalette(let darks):
             error = "Light theme doesn’t contains following assets: \(darks.joined(separator: ", ")), which exists in dark theme. Add these assets to light theme and publish to the Team Library."
         case .foundDuplicate(let assetName):
