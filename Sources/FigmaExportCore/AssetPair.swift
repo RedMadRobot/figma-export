72ff912b324a62ca87c1ae3beb64a9f7639e0e43
alexey1312 <alexey1312ru@gmail.com>
2022-03-29 11:05:31 +0600

    Fix AssetPair

--- Sources/FigmaExportCore/AssetPair.swift
+++ Sources/FigmaExportCore/AssetPair.swift
@@ -2,8 +2,8 @@ public struct AssetPair<AssetType> where AssetType: Asset {
     
     public let light: AssetType
     public let dark: AssetType?
-    public var lightHC: AssetType?
-    public var darkHC: AssetType?
+    public let lightHC: AssetType?
+    public let darkHC: AssetType?
     
     public init(
         light: AssetType,
@@ -14,14 +14,6 @@ public struct AssetPair<AssetType> where AssetType: Asset {
         self.light = light
         self.dark = dark
         self.lightHC = lightHC
-        self.darkHC = darkHC
-    }
-
-    public mutating func setLightHC(_ lightHC: AssetType?) {
-        self.lightHC = lightHC
-    }
-
-    public mutating func setDarkHC(_ darkHC: AssetType?) {
         self.darkHC = darkHC
     }
 }
