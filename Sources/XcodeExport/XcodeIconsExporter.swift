85e51ca8e526b539348e9531d6ab03f384a86e8d
Daniil Subbotin <mail@subdan.ru>
2022-05-13 17:26:48 +0300

    Add support for * symbol for `preservesVectorData`

--- Sources/XcodeExport/XcodeIconsExporter.swift
+++ Sources/XcodeExport/XcodeIconsExporter.swift
@@ -10,15 +10,17 @@ final public class XcodeIconsExporter: XcodeImagesExporterBase {
         // Generate assets
         let assetsFolderURL = output.assetsFolderURL
         let preservesVectorRepresentation = output.preservesVectorRepresentation
+        let filter = AssetsFilter(filters: preservesVectorRepresentation ?? [])
+
         // Filtering at suffixes
         let renderMode = output.renderMode ?? .template
         let defaultSuffix = renderMode == .template ? output.renderModeDefaultSuffix : nil
         let originalSuffix = renderMode == .template ? output.renderModeOriginalSuffix : nil
         let templateSuffix = renderMode != .template ? output.renderModeTemplateSuffix : nil
-
+        
         let imageAssetsFiles = try icons.flatMap { imagePack -> [FileContents] in
-            let preservesVector = preservesVectorRepresentation?.first(where: { $0 == imagePack.light.name }) != nil
-
+            let preservesVector = filter.match(name: imagePack.light.name)
+            
             if let defaultSuffix = defaultSuffix, imagePack.light.name.hasSuffix(defaultSuffix) {
                 return try imagePack.makeFileContents(to: assetsFolderURL, preservesVector: preservesVector, renderMode: .default)
             } else if let originalSuffix = originalSuffix, imagePack.light.name.hasSuffix(originalSuffix) {
@@ -29,7 +31,7 @@ final public class XcodeIconsExporter: XcodeImagesExporterBase {
 
             return try imagePack.makeFileContents(to: assetsFolderURL, preservesVector: preservesVector, renderMode: renderMode)
         }
-
+        
         // Generate extensions
         let imageNames = icons.map { normalizeName($0.light.name) }
         let extensionFiles = try generateExtensions(names: imageNames, append: append)
