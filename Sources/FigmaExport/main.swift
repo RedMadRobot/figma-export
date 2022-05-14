5a8bd6fee1fd6fc3697376ba6bcfba5b2392f045
Daniil Subbotin <d.subbotin@opends.tech>
2022-05-14 13:00:31 +0300

    0.34.0

--- Sources/FigmaExport/main.swift
+++ Sources/FigmaExport/main.swift
@@ -31,7 +31,7 @@ enum FigmaExportError: LocalizedError {
 
 struct FigmaExportCommand: ParsableCommand {
     
-    static let version = "0.33.0"
+    static let version = "0.34.0"
     
     static let svgFileConverter = VectorDrawableConverter()
     static let fileWriter = FileWriter()
