94eab4c17b36264884cd637e9a09670e93f57272
Daniil Subbotin <mail@subdan.ru>
2022-07-26 12:23:44 +0300

    0.35.1

--- Sources/FigmaExport/main.swift
+++ Sources/FigmaExport/main.swift
@@ -31,7 +31,7 @@ enum FigmaExportError: LocalizedError {
 
 struct FigmaExportCommand: ParsableCommand {
     
-    static let version = "0.35.0"
+    static let version = "0.35.1"
     
     static let svgFileConverter = VectorDrawableConverter()
     static let fileWriter = FileWriter()
