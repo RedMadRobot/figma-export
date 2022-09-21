b4dfecf9fb37c11b3b51f245307d310907e8c472
Edvald Eysteinson <edvald.eysteinson@Edvald-Eysteinsson-MBP.local>
2022-09-21 18:24:27 +0200

    fixed test

--- Tests/AndroidExportTests/AndroidTypographyExporterTests.swift
+++ Tests/AndroidExportTests/AndroidTypographyExporterTests.swift
@@ -56,7 +56,7 @@ final class AndroidTypographyExporterTests: XCTestCase {
                 <item name="android:letterSpacing">0.00</item>
             </style>
         </resources>
-        
+
         """
         XCTAssertNoDifference(generatedCode, referenceCode)
         
