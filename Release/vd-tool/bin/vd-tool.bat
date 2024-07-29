37753a2835b65cdb48b3cb1056ae5b2859e7960c
Nazar Rusnak <inazarrusnak@gmail.com>
2024-07-29 21:46:04 +0300

    Update vd-tool

--- Release/vd-tool/bin/vd-tool.bat
+++ Release/vd-tool/bin/vd-tool.bat
@@ -40,7 +40,7 @@ if defined JAVA_HOME goto findJavaFromJavaHome
 
 set JAVA_EXE=java.exe
 %JAVA_EXE% -version >NUL 2>&1
-if "%ERRORLEVEL%" == "0" goto init
+if "%ERRORLEVEL%" == "0" goto execute
 
 echo.
 echo ERROR: JAVA_HOME is not set and no 'java' command could be found in your PATH.
@@ -54,7 +54,7 @@ goto fail
 set JAVA_HOME=%JAVA_HOME:"=%
 set JAVA_EXE=%JAVA_HOME%/bin/java.exe
 
-if exist "%JAVA_EXE%" goto init
+if exist "%JAVA_EXE%" goto execute
 
 echo.
 echo ERROR: JAVA_HOME is set to an invalid directory: %JAVA_HOME%
@@ -64,29 +64,14 @@ echo location of your Java installation.
 
 goto fail
 
-:init
-@rem Get command-line arguments, handling Windows variants
-
-if not "%OS%" == "Windows_NT" goto win9xME_args
-
-:win9xME_args
-@rem Slurp the command line arguments.
-set CMD_LINE_ARGS=
-set _SKIP=2
-
-:win9xME_args_slurp
-if "x%~1" == "x" goto execute
-
-set CMD_LINE_ARGS=%*
-
 :execute
 @rem Setup the command line
 
-set CLASSPATH=%APP_HOME%\lib\vd-tool.jar;%APP_HOME%\lib\sdk-common-27.1.0.jar;%APP_HOME%\lib\sdklib-27.1.0.jar;%APP_HOME%\lib\builder-test-api-4.1.0.jar;%APP_HOME%\lib\builder-model-4.1.0.jar;%APP_HOME%\lib\ddmlib-27.1.0.jar;%APP_HOME%\lib\shared-27.1.0.jar;%APP_HOME%\lib\bcpkix-jdk15on-1.56.jar;%APP_HOME%\lib\bcprov-jdk15on-1.56.jar;%APP_HOME%\lib\repository-27.1.0.jar;%APP_HOME%\lib\layoutlib-api-27.1.0.jar;%APP_HOME%\lib\dvlib-27.1.0.jar;%APP_HOME%\lib\common-27.1.0.jar;%APP_HOME%\lib\kotlin-stdlib-jdk8-1.3.72.jar;%APP_HOME%\lib\kotlin-reflect-1.3.72.jar;%APP_HOME%\lib\aapt2-proto-4.1.0-alpha01-6193524.jar;%APP_HOME%\lib\protos-27.1.0.jar;%APP_HOME%\lib\protobuf-java-3.10.0.jar;%APP_HOME%\lib\javax.inject-1.jar;%APP_HOME%\lib\trove4j-20160824.jar;%APP_HOME%\lib\gson-2.8.5.jar;%APP_HOME%\lib\commons-compress-1.12.jar;%APP_HOME%\lib\httpmime-4.5.6.jar;%APP_HOME%\lib\httpclient-4.5.6.jar;%APP_HOME%\lib\httpcore-4.4.10.jar;%APP_HOME%\lib\annotations-27.1.0.jar;%APP_HOME%\lib\kxml2-2.3.0.jar;%APP_HOME%\lib\jimfs-1.1.jar;%APP_HOME%\lib\guava-28.1-jre.jar;%APP_HOME%\lib\kotlin-stdlib-jdk7-1.3.72.jar;%APP_HOME%\lib\kotlin-stdlib-1.3.72.jar;%APP_HOME%\lib\annotations-13.0.jar;%APP_HOME%\lib\javax.activation-1.2.0.jar;%APP_HOME%\lib\jaxb-runtime-2.3.1.jar;%APP_HOME%\lib\failureaccess-1.0.1.jar;%APP_HOME%\lib\listenablefuture-9999.0-empty-to-avoid-conflict-with-guava.jar;%APP_HOME%\lib\jsr305-3.0.2.jar;%APP_HOME%\lib\checker-qual-2.8.1.jar;%APP_HOME%\lib\error_prone_annotations-2.3.2.jar;%APP_HOME%\lib\j2objc-annotations-1.3.jar;%APP_HOME%\lib\animal-sniffer-annotations-1.18.jar;%APP_HOME%\lib\kotlin-stdlib-common-1.3.72.jar;%APP_HOME%\lib\jaxb-api-2.3.1.jar;%APP_HOME%\lib\txw2-2.3.1.jar;%APP_HOME%\lib\istack-commons-runtime-3.0.7.jar;%APP_HOME%\lib\stax-ex-1.8.jar;%APP_HOME%\lib\FastInfoset-1.2.15.jar;%APP_HOME%\lib\javax.activation-api-1.2.0.jar;%APP_HOME%\lib\commons-logging-1.2.jar;%APP_HOME%\lib\commons-codec-1.10.jar
+set CLASSPATH=%APP_HOME%\lib\vd-tool.jar;%APP_HOME%\lib\sdk-common-31.5.1.jar;%APP_HOME%\lib\sdklib-31.5.1.jar;%APP_HOME%\lib\repository-31.5.1.jar;%APP_HOME%\lib\shared-31.5.1.jar;%APP_HOME%\lib\ddmlib-31.5.1.jar;%APP_HOME%\lib\layoutlib-api-31.5.1.jar;%APP_HOME%\lib\dvlib-31.5.1.jar;%APP_HOME%\lib\common-31.5.1.jar;%APP_HOME%\lib\annotations-31.5.1.jar;%APP_HOME%\lib\aapt2-proto-8.5.1-11315950.jar;%APP_HOME%\lib\gson-2.10.1.jar;%APP_HOME%\lib\jimfs-1.1.jar;%APP_HOME%\lib\guava-32.0.1-jre.jar;%APP_HOME%\lib\protos-31.5.1.jar;%APP_HOME%\lib\protobuf-java-3.22.3.jar;%APP_HOME%\lib\javax.inject-1.jar;%APP_HOME%\lib\kxml2-2.3.0.jar;%APP_HOME%\lib\bcpkix-jdk18on-1.77.jar;%APP_HOME%\lib\bcutil-jdk18on-1.77.jar;%APP_HOME%\lib\bcprov-jdk18on-1.77.jar;%APP_HOME%\lib\jaxb-runtime-2.3.2.jar;%APP_HOME%\lib\trove4j-1.0.20200330.jar;%APP_HOME%\lib\kotlin-reflect-1.9.20.jar;%APP_HOME%\lib\kotlin-stdlib-jdk7-1.9.20.jar;%APP_HOME%\lib\kotlin-stdlib-1.9.20.jar;%APP_HOME%\lib\kotlin-stdlib-jdk8-1.9.20.jar;%APP_HOME%\lib\jna-platform-5.6.0.jar;%APP_HOME%\lib\annotations-23.0.0.jar;%APP_HOME%\lib\commons-compress-1.21.jar;%APP_HOME%\lib\httpmime-4.5.6.jar;%APP_HOME%\lib\httpclient-4.5.6.jar;%APP_HOME%\lib\httpcore-4.4.16.jar;%APP_HOME%\lib\failureaccess-1.0.1.jar;%APP_HOME%\lib\listenablefuture-9999.0-empty-to-avoid-conflict-with-guava.jar;%APP_HOME%\lib\jsr305-3.0.2.jar;%APP_HOME%\lib\checker-qual-3.33.0.jar;%APP_HOME%\lib\error_prone_annotations-2.18.0.jar;%APP_HOME%\lib\j2objc-annotations-2.8.jar;%APP_HOME%\lib\stax-ex-1.8.1.jar;%APP_HOME%\lib\jakarta.xml.bind-api-2.3.2.jar;%APP_HOME%\lib\txw2-2.3.2.jar;%APP_HOME%\lib\istack-commons-runtime-3.0.8.jar;%APP_HOME%\lib\FastInfoset-1.2.16.jar;%APP_HOME%\lib\jakarta.activation-api-1.2.1.jar;%APP_HOME%\lib\jna-5.6.0.jar;%APP_HOME%\lib\javax.activation-1.2.0.jar;%APP_HOME%\lib\commons-logging-1.2.jar;%APP_HOME%\lib\commons-codec-1.10.jar
 
 
 @rem Execute vd-tool
-"%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %VD_TOOL_OPTS%  -classpath "%CLASSPATH%" com.android.ide.common.vectordrawable.VdCommandLineTool %CMD_LINE_ARGS%
+"%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %VD_TOOL_OPTS%  -classpath "%CLASSPATH%" com.android.ide.common.vectordrawable.VdCommandLineTool %*
 
 :end
 @rem End local scope for the variables with windows NT shell
