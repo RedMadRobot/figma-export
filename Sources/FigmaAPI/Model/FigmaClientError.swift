018b53cf81e559b275cf438c9b2182ef8b7e1378
r3econ <505054+r3econ@users.noreply.github.com>
2025-05-20 12:52:23 +0200

    Enhance Error Messaging, Fix README Typos, and Improve Markdown Formatting (#257)
    * Improve error description and include more troubleshooting information
    
    * Fix typos
    
    * Update readme
    
    ---------
    
    Co-authored-by: Rafal Sroka <rafal.sroka@srf.com>

--- Sources/FigmaAPI/Model/FigmaClientError.swift
+++ Sources/FigmaAPI/Model/FigmaClientError.swift
@@ -7,7 +7,7 @@ struct FigmaClientError: Decodable, LocalizedError {
     var errorDescription: String? {
         switch err {
         case "Not found":
-            return "Figma file not found. Check lightFileId and darkFileId (if you project supports dark mode) in the yaml config file."
+            return "Figma file not found. Check lightFileId and darkFileId (if your project supports dark mode) in the yaml config file. Also verify that your personal access token is valid and hasn't expired."
         default:
             return "Figma API: \(err)"
         }
