238b7413e3e6604cfed07c1646f4a0ab013814ef
Alessio Arsuffi <alessio.arsuffi@ovolab.com>
2025-05-26 12:54:02 +0200

    use dateDecodingStrategy as iso8601

--- Sources/FigmaAPI/Endpoint/BaseEndpoint.swift
+++ Sources/FigmaAPI/Endpoint/BaseEndpoint.swift
@@ -39,6 +39,7 @@ extension JSONDecoder {
     internal static let `default`: JSONDecoder = {
         let decoder = JSONDecoder()
         decoder.keyDecodingStrategy = .convertFromSnakeCase
+		decoder.dateDecodingStrategy = .iso8601
         return decoder
     }()
 }
